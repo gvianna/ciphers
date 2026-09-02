5d29d1034f347300de54d0d2fc265e095c614943
Guilherme José Vianna <gjosevianna@gmail.com>
2026-09-02 15:01:07 -0300

    Implement Atbash cipher function
    Added Atbash cipher function to encrypt text.

--- algorithms/ciphers.py
+++ algorithms/ciphers.py
@@ -16,3 +16,24 @@ def caesar_cipher(text: str, shift_value: int) -> str:
             code = first + wrapped_position
             cipher += chr(code)
     return cipher
+
+
+# Atbash Cipher
+def atbash_cipher(text: str) -> str:
+    cipher = ""
+
+    for char in text:
+        if not char.isalpha():
+            cipher += char
+        else:
+            if char.isupper():
+                first = ord("A")
+            else:
+                first = ord("a")
+
+            position = ord(char) - first
+            atbash_position = 25 - position
+            code = first + atbash_position
+            cipher += chr(code)
+
+    return cipher
